e8c275d545816fb13e62e0cd2352e1cd14703536
Jay <jdmon1998>
2020-10-05 23:10:51 -0400

    making progress on the assignment

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const swagger = require('swagger-ui-express');
 const app = express();
 const port = 3000;
 const pool = require('./db');
+const docs = require('./swagger.json')
 
 app.get('/agents', async (req, res) => {
 	let conn;
 	try {
 		conn = await pool.getConnection();
-		
+
 		var query = "SELECT * from agents";
 		var rows = await conn.query(query);
 		res.setHeader('Content-Type', 'application/json');
@@ -23,10 +25,10 @@ app.get('/student', async (req, res) => {
 	let conn;
 	try {
 		conn = await pool.getConnection();
-		
+
 		var query = "SELECT * from student";
 		var rows = await conn.query(query);
-		
+
 		res.setHeader('Content-Type', 'application/json');
 		res.json(rows);
 	} catch (err) {
@@ -40,10 +42,10 @@ app.get('/customer', async (req, res) => {
 	let conn;
 	try {
 		conn = await pool.getConnection();
-		
+
 		var query = "SELECT * from customer";
 		var rows = await conn.query(query);
-		
+
 		res.setHeader('Content-Type', 'application/json');
 		res.json(rows);
 	} catch (err) {
@@ -52,6 +54,8 @@ app.get('/customer', async (req, res) => {
 		if (conn) return conn.release();
 	}
 });
+
+app.use('/docs', swagger.serve, swagger.setup(docs));
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
